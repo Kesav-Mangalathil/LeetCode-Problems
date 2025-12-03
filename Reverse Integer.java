e03b7e99e409fb66952d7f4bfd2254f4ffe62e50
kesav <kesav5470@gmail.com>
2025-12-03 12:00:00 +0400

    Reverse Integer

---
+++ Reverse Integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int temp = x;
+        int rev = 0;
+        while(temp!=0 ){
+        rev=rev*10 + temp%10;
+        temp=temp/10;
+            
+        }
+        return rev;
+    
+        
+        
+    }}
+
