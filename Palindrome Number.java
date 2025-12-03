c7b110ba9dd0cd3bd6c6cabaeeb902ce1d93c265
kesav <kesav5470@gmail.com>
2025-12-03 12:00:00 +0400

    Palindrome Number

---
+++ Palindrome Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        if( x <0 )
+        return false;
+        while(temp>0){
+            rev = rev*10 + temp%10;
+            temp = temp/10;
+        }
+        if(rev==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
