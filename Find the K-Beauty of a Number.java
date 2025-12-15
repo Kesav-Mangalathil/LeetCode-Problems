6e5832b57ee9b21017b5bd88f85cb338975739a4
kesav <kesav5470@gmail.com>
2025-12-15 12:00:00 +0400

    Find the K-Beauty of a Number

---
+++ Find the K-Beauty of a Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+           String s = String.valueOf(num);
+        int n = s.length();
+        int ans = 0;
+
+        for (int i = 0; i + k <= n; i++) {
+            int val = Integer.parseInt(s.substring(i, i + k));
+            if (val != 0 && num % val == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
